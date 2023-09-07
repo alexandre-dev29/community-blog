fea666aa1ec9569073b0fae5a7f410566d3d4ceb
alexandre-dev28 <axel.business29@gmail.com>
2023-09-07 14:33:21 +0200

    little reformat
    Signed-off-by: alexandre-dev28 <axel.business29@gmail.com>

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,11 +1,11 @@
-import {NestApplication, NestFactory} from '@nestjs/core';
-import {AppModule} from './app.module';
-import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
+import { NestApplication, NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestApplication>(AppModule, {
-    cors: {credentials: true, origin: 'https://blogcommunity.axelmwenze.dev'},
+    cors: { credentials: true, origin: 'https://blogcommunity.axelmwenze.dev' },
   });
   const port = process.env.PORT;
 
