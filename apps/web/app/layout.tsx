1ed733aeabfdce631ac8d8a0a4f542b37344b160
alexandre-dev28 <axel.business29@gmail.com>
2023-09-23 15:11:18 +0200

    small changes to toggle build process
    Signed-off-by: alexandre-dev28 <axel.business29@gmail.com>

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,16 +1,16 @@
 import "@/styles/globals.css"
-import {Metadata} from "next"
-import {cookies} from "next/headers"
+import { Metadata } from "next"
+import { cookies } from "next/headers"
 
 import RefineConfig from "@/config/refine-config"
-import {siteConfig} from "@/config/site"
-import {getAllCategories, getAllPostsForSearch} from "@/lib/api-calls"
-import {fontSans} from "@/lib/fonts"
-import {cn} from "@/lib/utils"
-import {Toaster} from "@/components/ui/toaster"
-import {CommandRender} from "@/components/command/command-render"
-import {TailwindIndicator} from "@/components/tailwind-indicator"
-import {ThemeProvider} from "@/components/theme-provider"
+import { siteConfig } from "@/config/site"
+import { getAllCategories, getAllPostsForSearch } from "@/lib/api-calls"
+import { fontSans } from "@/lib/fonts"
+import { cn } from "@/lib/utils"
+import { Toaster } from "@/components/ui/toaster"
+import { CommandRender } from "@/components/command/command-render"
+import { TailwindIndicator } from "@/components/tailwind-indicator"
+import { ThemeProvider } from "@/components/theme-provider"
 
 export const metadata: Metadata = {
   title: {
@@ -19,8 +19,8 @@ export const metadata: Metadata = {
   },
   description: siteConfig.description,
   themeColor: [
-    {media: "(prefers-color-scheme: light)", color: "white"},
-    {media: "(prefers-color-scheme: dark)", color: "black"},
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
   ],
   metadataBase: new URL(`${process.env.NEXT_PUBLIC_FRONT_URL}`),
   icons: {
@@ -42,29 +42,30 @@ interface RootLayoutProps {
   children: React.ReactNode
 }
 
-export default async function RootLayout({children}: RootLayoutProps) {
+export default async function RootLayout({ children }: RootLayoutProps) {
   const [post, listOfCategories] = await Promise.all([
     getAllPostsForSearch(cookies().toString()),
     getAllCategories(cookies().toString()),
   ])
+
   return (
     <>
       <html lang="en" suppressHydrationWarning>
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-      <RefineConfig>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          {children}
-          <TailwindIndicator/>
-          <CommandRender categories={listOfCategories} posts={post}/>
-          <Toaster/>
-        </ThemeProvider>
-      </RefineConfig>
-      </body>
+        <body
+          className={cn(
+            "min-h-screen bg-background font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          <RefineConfig>
+            <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+              {children}
+              <TailwindIndicator />
+              <CommandRender categories={listOfCategories} posts={post} />
+              <Toaster />
+            </ThemeProvider>
+          </RefineConfig>
+        </body>
       </html>
     </>
   )
